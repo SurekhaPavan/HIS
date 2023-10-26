f1936e6485b9cf02e64618f7f4e45eac79182af6
Sri Surekha <9412rs@gmail.com>
2023-10-25 23:16:17 -0500

    added new data & data files

---
+++ DoctorDATA.sql
@@ -0,0 +1,30 @@
+select * from doctor;
+
+INSERT INTO Doctor (DoctorId, DoctorFName, DoctorLName, Specialization, Phone, Email) VALUES
+('D002', 'Emily', 'Cooper', 'Pediatrician', '234-567-8901', 'emily.Cooper@gmail.com');
+INSERT INTO Doctor (DoctorId, DoctorFName, DoctorLName, Specialization, Phone, Email) VALUES
+('D003', 'Michael', 'Davis', 'Orthopedic Surgeon', '345-678-9012', 'michael.davis@gmail.com');
+INSERT INTO Doctor (DoctorId, DoctorFName, DoctorLName, Specialization, Phone, Email) VALUES
+('D004', 'Jessica', 'Lee', 'Dermatologist', '456-789-0123', 'Jessica.lee@gmail.com');
+INSERT INTO Doctor (DoctorId, DoctorFName, DoctorLName, Specialization, Phone, Email) VALUES
+('D005', 'Daniel', 'Brown', 'Neurologist', '567-890-1234', 'Daniel.brown@gmail.com');
+INSERT INTO Doctor (DoctorId, DoctorFName, DoctorLName, Specialization, Phone, Email) VALUES
+('D006', 'Amanda', 'White', 'Gynecologist', '678-901-2345', 'amanda.white@gmail.com');
+INSERT INTO Doctor (DoctorId, DoctorFName, DoctorLName, Specialization, Phone, Email) VALUES
+('D007', 'Christopher', 'Hall', 'Oncologist', '789-012-3456', 'christopher.hall@gmail.com');
+INSERT INTO Doctor (DoctorId, DoctorFName, DoctorLName, Specialization, Phone, Email) VALUES
+('D008', 'Sophia', 'Clark', 'Psychiatrist', '890-123-4567', 'Sophia.clark@gmail.com');
+INSERT INTO Doctor (DoctorId, DoctorFName, DoctorLName, Specialization, Phone, Email) VALUES
+('D009', 'Daniel', 'Baker', 'Urologist', '901-234-5678', 'daniel.baker@gmail.com');
+INSERT INTO Doctor (DoctorId, DoctorFName, DoctorLName, Specialization, Phone, Email) VALUES
+('D010', 'Wilson', 'Turner', 'Endocrinologist', '012-345-6789', 'Wilson.turner@gmail.com');
+INSERT INTO Doctor (DoctorId, DoctorFName, DoctorLName, Specialization, Phone, Email) VALUES
+('D011', 'Matthew', 'Lopez', 'Rheumatologist', '135-246-3579', 'matthew.lopez@gmail.com');
+INSERT INTO Doctor (DoctorId, DoctorFName, DoctorLName, Specialization, Phone, Email) VALUES
+('D012', 'Sophia', 'Hill', 'Dentist', '246-357-4680', 'sophia.hill@gmail.com');
+INSERT INTO Doctor (DoctorId, DoctorFName, DoctorLName, Specialization, Phone, Email) VALUES
+('D013', 'Ethan', 'Cooper', 'Gastroenterologist', '357-468-5791', 'ethan.cooper@gmail.com');
+INSERT INTO Doctor (DoctorId, DoctorFName, DoctorLName, Specialization, Phone, Email) VALUES
+('D014', 'Amelia', 'Ward', 'Pulmonologist', '468-579-6802', 'Amelia.ward@gmail.com');
+INSERT INTO Doctor (DoctorId, DoctorFName, DoctorLName, Specialization, Phone, Email) VALUES
+('D015', 'Zoe','Parker', 'Dermatopathologist', '579-680-7913', 'Zoe.parker@gmail.com');
